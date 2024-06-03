6fcf19770208ab72a8bb9339cb05eabde6e87504
Besik Kavzharadze <b.kavzharadze@gmail.com>
2024-06-03 21:35:10 +0200

    update web packterser-webpack-plugin

--- next.config.mjs
+++ next.config.mjs
@@ -21,17 +21,17 @@ const withPWA = withPWAInit({
     //   },
 });
 
-export default withPWA({  
-});
+// export default withPWA({  
+// });
 
 
 // back up module
-// export default withPWA({
-//   webpack: (config, { dev, isServer }) => {
-//     if (!dev && !isServer) {
-//       config.optimization.minimizer.push(new TerserPlugin());
-//     }
-//     return config;
-//   },
+export default withPWA({
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.optimization.minimizer.push(new TerserPlugin());
+    }
+    return config;
+  },
 
-// });
+});
