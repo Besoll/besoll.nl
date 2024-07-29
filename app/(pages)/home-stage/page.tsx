36abe3ce3292a5ae3ba59b1998c1a996474891c7
Besik Kavzharadze <b.kavzharadze@gmail.com>
2024-07-29 12:41:09 +0200

    and now? update all

--- app/(pages)/home-stage/page.tsx
+++ app/(pages)/home-stage/page.tsx
@@ -1,15 +1,14 @@
 import {Hero} from "@/components/home/Hero2"
 
 import dynamic from 'next/dynamic'
-const OurStudio = dynamic(() => import('@/components/home/OurStudio')) 
-const OurServices = dynamic(() => import('@/components/home/OurServices')) 
-const HowWeBuild = dynamic(() => import('@/components/home/HowWeBuild')) 
-const Grid = dynamic(() => import('@/components/home/Grid')) 
-const RecentProjects = dynamic(() => import('@/components/home/RecentProjects')) 
-const Clients = dynamic(() => import('@/components/home/Clients')) 
-const Exprerience = dynamic(() => import('@/components/home/Exprerience')) 
-const Approach = dynamic(() => import('@/components/home/Approach')) 
-const FormMain = dynamic(() => import('@/components/ui/Form')) 
+// const OurStudio = dynamic(() => import('@/components/home/OurStudio')) 
+// const OurServices = dynamic(() => import('@/components/home/OurServices')) 
+// const HowWeBuild = dynamic(() => import('@/components/home/HowWeBuild')) 
+// const Grid = dynamic(() => import('@/components/home/Grid')) 
+// const RecentProjects = dynamic(() => import('@/components/home/RecentProjects')) 
+// const Exprerience = dynamic(() => import('@/components/home/Exprerience')) 
+// const Approach = dynamic(() => import('@/components/home/Approach')) 
+// const FormMain = dynamic(() => import('@/components/ui/Form')) 
 
 
 export default function Home() {
