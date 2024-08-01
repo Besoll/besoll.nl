1e3c7ca53c4e79e96d82e99971c295ff483af431
Besik Kavzharadze <b.kavzharadze@gmail.com>
2024-08-01 11:23:38 +0200

    update

--- app/(pages)/our-team/layout.tsx
+++ app/(pages)/our-team/layout.tsx
@@ -6,7 +6,7 @@ const Footer = dynamic(() => import('@/components/global/Footer') )
 const titleBL = 'OwlWeb - our team';
 const colorBL = '#FDA800'; // orange
 const urlBL = 'https://owlweb.nl/our-team';
-const imageBL = 'https://www.owlweb.nl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F1.a83945c5.webp&w=1080&q=75';
+const imageBL = 'https://www.owlweb.nl/_next/image?url=%2Fowlweb-studio.webp&w=1920&q=75';
 const descriptionBL = 'Welcome to OwlWeb Studio! Meet the fun-loving crew who turn your web dreams into reality. We are a quirky bunch of tech enthusiasts and creative geniuses dedicated to crafting awesome web experiences.';
 
 const layout = ({ children }: { children: React.ReactNode }) => {
